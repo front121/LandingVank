962eff780da73027afb2aea98f9c40217551f3aa
Daniel Aldana <frontend@thisisvank.com>
2024-02-09 17:00:05 -0500

    add lazy Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import "./App.css";
-import Home from "./Page/Home/Home";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import ImageLoading from "./Page/ImageLoading";
+
+const Home = React.lazy(() => import('./Page/Home/Home'));
 
 function App() {
-  return <Home />;
+  return (
+    <Suspense fallback={<ImageLoading />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Suspense>
+  );
 }
-
 export default App;
---
+++ src/Page/ImageLoading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageLoading = () => {
+  return (
+    <div>
+      <h2>Loading Images...</h2>
+      {/* Agregar aquí un spinner u otro indicador de carga */}
+    </div>
+  );
+};
+
+export default ImageLoading;
